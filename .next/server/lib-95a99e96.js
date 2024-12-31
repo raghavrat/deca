a2cd7bcfbf6f6db685abe786512d5058f185b27b
doggysir <78118570+doggysir@users.noreply.github.com>
2024-12-30 22:55:26 -0500

    dynamic

---
+++ .next/server/lib-95a99e96.js
@@ -0,0 +1,1 @@
+"use strict";exports.id=381,exports.ids=[381],exports.modules={1474:(s,e,t)=>{}};
--- next.config.js
+++ next.config.js
@@ -1,37 +1,54 @@
 /** @type {import('next').NextConfig} */
+const crypto = require('crypto');
+
 const nextConfig = {
   output: 'standalone',
   experimental: {
     optimizePackageImports: ['lucide-react'],
   },
-  webpack: (config, { isServer }) => {
-    // Optimize bundle size
+  webpack: (config) => {
     config.optimization = {
       ...config.optimization,
-      minimize: true,
+      splitChunks: {
+        chunks: 'all',
+        minSize: 20000,
+        maxSize: 90000,
+        cacheGroups: {
+          default: false,
+          vendors: false,
+          framework: {
+            chunks: 'all',
+            name: 'framework',
+            test: /(?<!node_modules.*)[\\/]node_modules[\\/](react|react-dom|scheduler|prop-types|use-subscription)[\\/]/,
+            priority: 40,
+            enforce: true
+          },
+          commons: {
+            name: 'commons',
+            chunks: 'initial',
+            minChunks: 2,
+            priority: 20
+          },
+          lib: {
+            test(module) {
+              return module.size() > 50000 &&
+                /node_modules[/\\]/.test(module.identifier())
+            },
+            name(module) {
+              const hash = crypto.createHash('sha1')
+              hash.update(module.identifier())
+              return 'lib-' + hash.digest('hex').substring(0, 8)
+            },
+            priority: 30,
+            minChunks: 1,
+            reuseExistingChunk: true
+          }
+        }
+      }
     }
-
     return config
   },
-  compress: true,
+  compress: true
 }
-module.exports = {
-    compress: true,
-    webpack: (config) => {
-      config.optimization = {
-        ...config.optimization,
-        splitChunks: {
-          chunks: 'all',
-          maxSize: 400000,
-          cacheGroups: {
-            vendor: {
-              test: /[\\/]node_modules[\\/]/,
-              chunks: 'all',
-              maxSize: 400000
-            },
-          },
-        },
-      }
-      return config
-    }
-  }
+
+module.exports = nextConfig
